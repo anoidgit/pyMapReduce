051ab679125cd08e029d39707574947220d087ac
ano <anoidgit@users.noreply.github.com>
2017-04-30 09:00:53 +0800

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -4,13 +4,15 @@ import sys
 
 import zmq, json
 
-from mrmethods import mapcore
+import mapcore
+
+mapfunc = mapcore.mapfunc
 
 def loadServer(bindaddr):
 	socket = zmq.Context().socket(zmq.PUB)
 	socket.bind(bindaddr)
 	while True:
-		socket.send(json.dumps(mapcore(json.loads(socket.recv()))))
+		socket.send(json.dumps(mapfunc(json.loads(socket.recv()))))
 
 if __name__ == "__main__":
 	loadServer(sys.argv[1].decode("utf-8"))
