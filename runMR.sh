2d144629cf4cb6e85665f34e504505208e98c69e
ano <anoidgit@users.noreply.github.com>
2017-04-30 19:55:21 +0800

    Update runMR.sh

--- runMR.sh
+++ runMR.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cachebatch = 72
+batchperunit = 36
+nthreads = 6
+
 #if you want to use pypy, use py rather than pyo
 python -O -m py_compile *.py 
 
@@ -10,7 +14,7 @@ python mapper.pyo 127.0.0.1:20004 &
 python mapper.pyo 127.0.0.1:20005 &
 python mapper.pyo 127.0.0.1:20006 &
 
-python reducer.pyo weibo.txt segrs.txt 128 32 6 127.0.0.1:20001 127.0.0.1:20002 127.0.0.1:20003 127.0.0.1:20004 127.0.0.1:20005 127.0.0.1:20006
+python reducer.pyo weibo.txt segrs.txt $cachebatch $batchperunit $nthreads 127.0.0.1:20001 127.0.0.1:20002 127.0.0.1:20003 127.0.0.1:20004 127.0.0.1:20005 127.0.0.1:20006
 
 # Warning: this command will kill all python process, better comment it
 #pkill python
