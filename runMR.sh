53864ce5149c920832bd5f9c4132f4472b7322ee
ano <anoidgit@users.noreply.github.com>
2017-04-30 18:33:48 +0800

    Update runMR.sh

--- runMR.sh
+++ runMR.sh
@@ -5,4 +5,7 @@ python mapper.py 0.0.0.0:20002 &
 python mapper.py 0.0.0.0:20003 &
 python mapper.py 0.0.0.0:20004 &
 
-python reducer.py weibo.txt segrs.txt 64 64 1 127.0.0.1:20001 0.0.0.0:20002 127.0.0.1:20003 0.0.0.0:20004 &
+python reducer.py weibo.txt segrs.txt 128 32 1 127.0.0.1:20001 0.0.0.0:20002 127.0.0.1:20003 0.0.0.0:20004
+
+# Warning: this command will kill all python process, better comment it
+#pkill python
