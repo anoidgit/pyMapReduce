db4e69c8648f4b74c199cd91e2a6f9c59f106557
ano <anoidgit@users.noreply.github.com>
2017-04-30 18:10:13 +0800

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -9,7 +9,7 @@ import mapcore
 mapfunc = mapcore.mapfunc
 
 def loadServer(bindaddr):
-	socket = zmq.Context().socket(zmq.PUB)
+	socket = zmq.Context().socket(zmq.REP)
 	if not bindaddr.startswith("tcp://"):
 		bindaddr = "tcp://"+bindaddr
 	socket.bind(bindaddr)
