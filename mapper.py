126cd6282be6545f9a6ac0de6094f75a55e60f70
ano <anoidgit@users.noreply.github.com>
2017-04-30 18:35:23 +0800

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -13,8 +13,15 @@ def loadServer(bindaddr):
 	if not bindaddr.startswith("tcp://"):
 		bindaddr = "tcp://"+bindaddr
 	socket.bind(bindaddr)
-	while True:
-		socket.send(json.dumps(mapfunc(json.loads(socket.recv()))))
+	runsgn = True
+	while runsgn:
+		req = json.loads(socket.recv())
+		if isinstance(req, str) or isinstance(req, unicode):
+			if req.lower().startswith("term"):
+				runsgn = False
+				socket.send(json.dumps("EXIT"))
+		else:
+			socket.send(json.dumps(mapfunc(req)))
 
 if __name__ == "__main__":
 	loadServer(sys.argv[1].decode("utf-8"))
